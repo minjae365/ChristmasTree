a87eb95eea414b3c4a7ebd85a612cb4d1e57b75b
minjae365 <51438465+minjae365@users.noreply.github.com>
2019-12-23 00:32:42 -0500

    Update chirstmastree.py

--- chirstmastree.py
+++ chirstmastree.py
@@ -1,4 +1,9 @@
 #given a number, create a christmas tree using only for and if statements
 tree_height = 4
+tree_root = 2
 for i in range(tree_number):
   print((tree_number - i - 1) * ' ' + (2*i + 1) * '*')
+  #print((leaf - i - 1)*' ' + '*' + (i*2 + 1)*'='+'*')
+  #if wanna do second row, change tree_number - i -1 to tree_number - i
+  for j in range(tree_root):
+    print((leaf - 1)* ' ' + '|')
